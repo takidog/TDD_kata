1bb60dcdcc5aeaa76a08101b000c7135f665ef8b
Takidog <wow@taki.dog>
2022-05-05 21:23:01 +0800

    Add buy many book test case

--- src/tests/test_book_shop.py
+++ src/tests/test_book_shop.py
@@ -55,4 +55,66 @@ def test_buy_one():
 
 
 def test_buy_many():
-    pass
+    shop = BookShop()
+
+    book_item_p1 = shop.add_items(name="Potter Part.1", price=100, quota=10)
+    book_item_p2 = shop.add_items(name="Potter Part.2", price=100, quota=3)
+    book_item_p3 = shop.add_items(name="Potter Part.3", price=100, quota=5)
+    book_item_p4 = shop.add_items(name="Potter Part.4", price=100, quota=7)
+    book_item_p5 = shop.add_items(name="Potter Part.5", price=100, quota=8)
+
+    pre_checkout = shop.pre_checkout(
+        item_list=[
+            {"item_id": book_item_p1.id, "quota": 5},
+            {"item_id": book_item_p2.id, "quota": 2},
+            {"item_id": book_item_p4.id, "quota": 2},
+        ],
+    )
+
+    assert pre_checkout.price == 100 * 9 * 0.9
+    assert len(pre_checkout.items) == 3
+    assert isinstance(pre_checkout.checkout_id, UUID)
+
+    receipt = shop.checkout(checkout_id=pre_checkout.checkout_id)
+
+    assert receipt.price == 100 * 9 * 0.9
+    assert len(receipt.items) == 3
+    assert isinstance(receipt.receipt_id, UUID)
+    assert isinstance(receipt.checkout_id, UUID)
+
+    # over quota
+
+    pre_checkout = shop.pre_checkout(
+        item_list=[
+            {"item_id": book_item_p3.id, "quota": 99},
+        ],
+    )
+
+    assert pre_checkout.price == 100 * 99
+    assert len(pre_checkout.items) == 1
+    assert isinstance(pre_checkout.checkout_id, UUID)
+    try:
+        receipt = shop.checkout(checkout_id=pre_checkout.checkout_id)
+    except ValueError as e:
+        assert e.args[0] == "Quota not enough"
+
+    pre_checkout = shop.pre_checkout(
+        item_list=[
+            {"item_id": book_item_p1.id, "quota": 4},
+            {"item_id": book_item_p2.id, "quota": 1},
+            {"item_id": book_item_p3.id, "quota": 1},
+            {"item_id": book_item_p4.id, "quota": 1},
+            {"item_id": str(book_item_p5.id), "quota": 1},
+        ],
+    )
+
+    assert pre_checkout.price == 100 * 8 * 0.75
+    assert len(pre_checkout.items) == 5
+    assert isinstance(pre_checkout.checkout_id, UUID)
+
+    receipt = shop.checkout(checkout_id=pre_checkout.checkout_id)
+
+    assert receipt.price == 100 * 8 * 0.75
+    assert len(receipt.items) == 5
+    assert isinstance(receipt.receipt_id, UUID)
+    assert isinstance(receipt.checkout_id, UUID)
