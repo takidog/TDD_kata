028b118be8689caa354b3eae64d6b0b203f053b1
Takidog <wow@taki.dog>
2022-05-05 18:35:04 +0800

    Add first case test (add items)

--- src/tests/test_book_shop.py
+++ src/tests/test_book_shop.py
@@ -1,5 +1,20 @@
 from controller.book_shop import BookShop
 
 
-def test_hello():
+def test_product_listing():
     shop = BookShop()
+
+    book_item = shop.add_items(
+        name="Potter Part.1"
+        price=100
+        quota=10
+    )
+
+    assert book_item.name == "Potter Part.1"
+
+    assert book_item.price == 100
+    
+    assert book_item.quota == 10
+
+    assert shop.get_items(book_id=book_item.id) == book_item.id
+    
