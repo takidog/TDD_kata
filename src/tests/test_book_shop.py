c72da6ac61e22aec625fe3ff50227fd7dd9a3eaf
Takidog <wow@taki.dog>
2022-05-05 19:16:05 +0800

    Add book model test case

--- src/tests/test_book_shop.py
+++ src/tests/test_book_shop.py
@@ -1,4 +1,5 @@
-from controller.book_shop import BookShop
+from controller.book_shop import BookShop, Book
+from uuid import UUID
 
 
 def test_product_listing():
@@ -13,3 +14,18 @@ def test_product_listing():
     assert book_item.quota == 10
 
     assert shop.get_item(book_id=book_item.id).id == book_item.id
+
+
+def test_book_model():
+    book = Book(name="test", price=300, quota=10)
+    assert book.name == "test"
+    assert book.price == 300
+    assert book.quota == 10
+
+    assert isinstance(book.id, UUID)
+
+    book_dict = book.to_dict()
+    assert isinstance(book_dict, dict)
+
+    required_keys = ["id", "name", "price", "quota"]
+    assert all([k in book_dict.keys() for k in required_keys])
